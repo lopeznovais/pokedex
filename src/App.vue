3b7edaec088cecd2eed32c1a5ac03bdb09dfa5af
Lopez <42989437+lopeznovais@users.noreply.github.com>
2021-05-08 13:54:25 -0400

    Commit Inicial

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,66 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="column is-half is-offset-one-quarter">
+      <input
+        class="input is-rounded"
+        type="text"
+        placeholder="Buscar pokémon pelo nome"
+        v-model="busca"
+      />
+      <button
+        class="button is-fullwidth is-success"
+        id="buscaBtn"
+        @click="buscar"
+      >
+        Buscar
+      </button>
+      <div v-for="poke in filteredPokemons" :key="poke.url">
+        <Pokemon
+          :name="poke.name"
+          :url="poke.url"
+          :num="poke.url.split('/')[6]"
+        />
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import axios from "axios";
+import Pokemon from "./components/Pokemon";
 export default {
-  name: 'App',
+  name: "App",
+  data() {
+    return {
+      pokemons: [],
+      filteredPokemons: [],
+      busca: "",
+    };
+  },
+  created: function() {
+    axios
+      .get("https://pokeapi.co/api/v2/pokemon?limit=151&offset=0")
+      .then((res) => {
+        this.pokemons = res.data.results;
+        this.filteredPokemons = res.data.results;
+      });
+  },
   components: {
-    HelloWorld
-  }
-}
+    Pokemon,
+  },
+  methods: {
+    buscar: function() {
+      if (this.busca == "" || this.busca == " ") {
+        this.filteredPokemons = this.pokemons;
+      } else {
+        this.filteredPokemons = this.pokemons.filter((pokemon) =>
+          pokemon.name.includes(this.busca.toLowerCase())
+        );
+      }
+    },
+  },
+};
 </script>
-
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
@@ -24,5 +69,9 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+#buscaBtn {
+  margin-top: 15px;
 }
 </style>
